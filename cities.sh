52de1583e5264fc333a0bb90c4f4252717e1a516
Alex T <lexkie@proton.me>
2025-09-17 09:26:35 -0400

    Add files via upload

--- cities.sh
+++ cities.sh
@@ -10,12 +10,13 @@ mapfile -t key_lat < <(jq --arg sname "$cname" -r '.[] | select(.name == $sname)
 mapfile -t key_long < <(jq --arg sname "$cname" -r '.[] | select(.name == $sname) | .lng' world_cities5000.json)
 numberofcities=0
 for cities in "${your_array[@]}"; do
-  echo $numberofcities ${countries[$numberofcities]} "$cities" 
-((numberofcities++))
+  echo -e "\e[38;2;173;255;47m  $numberofcities ${countries[$numberofcities]} $cities \e[0m"
+  ((numberofcities++))
 done
 echo "Please choose city:"
 read citynumber
 echo "-----"
+echo -e "\e[38;2;173;255;47mCity:\e[0m"
 echo ${countries[$citynumber]} ${your_array[$citynumber]}
 if [[ $key_lat == -* ]];then
 lat="${key_lat:1}"
@@ -33,22 +34,22 @@ if [[ $key_long != -* ]];then
 lng="${key_long}"
 lng+="E"
 fi
-echo "Sunrise:"
+echo -e "\e[38;2;173;255;47m" "Sunrise:" "\e[0m"
 sunwait list rise $lat $long
-echo "Sunset:"
+echo -e "\e[38;2;173;255;47m" "Sunset:" "\e[0m"
 sunwait list sunset $lat $long
-echo "TimeZone:"
+echo -e "\e[38;2;173;255;47m" "TimeZone:" "\e[0m"
 #cargo build --no-default-features
 #cargo add tzf-rs --no-default-features
 bodhi=$(tzf --lng ${key_long[$citynumber]} --lat ${key_lat[$citynumber]})
 temp="${bodhi#\"}"
 result="${temp%\"}"
 echo "$result"
-echo "Offset:"
+echo -e "\e[38;2;173;255;47m" "Offset:" "\e[0m"
 mapfile -t other_offset < <(jq --arg oname "$result" -r '.[] | select(.timezone_id == $oname) | .dst_offset' timezonesx.json)
 myoffset="${other_offset[@]}"
 echo "$myoffset"
-echo "Time:"
+echo -e "\e[38;2;173;255;47m" "Time:" "\e[0m"
 if [[ $myoffset == -* ]];then
 offset=$myoffset
 fi
@@ -57,7 +58,7 @@ offset="$myoffset"
 fi
 myfinaloffset="$offset"
 TZ=UTC date --date="$myfinaloffset hours"
-echo "done"
+echo -e "\e[38;2;173;255;47m" "done" "\e[0m"
 sleep 10
 
 
